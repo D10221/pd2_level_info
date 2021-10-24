00fd060cc0fc528ce5e00491b79a844d487da276
d10221 <d10221@protonmail.com>
2021-10-24 08:53:47 +0200

    ... ignores

--- src/util/_util.lua
+++
@@ -1,9 +0,0 @@
-local function readAll(filePath)
-    local f = assert(io.open(filePath, "rb"))
-    local content = f:read("*all")
-    f:close()
-    return content
-end
-
-local _mod = json.decode(readAll(ModPath .. "mod.txt"))
-log(json.encode(_mod))
