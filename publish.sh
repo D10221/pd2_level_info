54c71f1f8cd592f6ac0b5127e847d5b54df9e2ef
d10221 <d10221@protonmail.com>
2021-10-24 08:52:44 +0200

    typo

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 # package:
-rm dist/ -rf && mkdir dist && zip -r dist/pd2_vele_info.zip src/**/* icon.png mod.txt
+rm dist/ -rf && mkdir dist && zip -r dist/pd2_level_info.zip src/**/* icon.png mod.txt
